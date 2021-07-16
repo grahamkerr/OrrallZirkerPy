1ac8b041407a06c1acf240daa2bc66c76008d686
Graham Kerr <gskerr89@gmail.com>
2021-07-15 23:27:23 -0400

    updating CrossSections.py and uploading figs

--- CrossSections.py
+++ CrossSections.py
@@ -1,5 +1,7 @@
 import numpy as np 
-
+from numpy.polynomial.polynomial import Polynomial as Poly
+from numpy.polynomial.chebyshev import Chebyshev as Cheb
+from scipy.optimize import curve_fit
 
 class CrossSec:
 
@@ -30,6 +32,8 @@ class CrossSec:
 
         self.energy = energy
         self.nE = len(self.energy)
+
+
 
     def cs_bw99(self):
 
@@ -564,3 +568,230 @@ class cs_bates53:
                                     -16.71))*np.pi*a0**2*1e17
         self.Q_p3 = self.Q_p_3s + self.Q_p_3p + self.Q_p_3d
         self.units = 'energy in [keV], Q in [10^-17 cm^-2]'
+
+
+class cs_winter09:
+
+    '''
+    This class holds the energy and cross sections for charge transfer 
+    from Winter 2009 Phys. Rev. A. 80, Table 5. Essentially an update 
+    to Shakeshaft 1978.
+   
+    Cross sections are of collisions between protons and neutral H to 
+
+    1s : Q_p1
+    2s : Q_p_2s
+    2p : Q_p_2p
+    n=2: sum of 2s and 2p states, Q_p2
+    3s : Q_p_3s
+    3p : Q_p_3p
+    3d : Q_p_3d
+    n=3 : sum of 3s, 3p, 3d states, Q_p3
+ 
+    Energy range  1 -- 100 keV
+
+    '''
+
+    def __init__(self):
+        
+        self.energy = np.array((1.0, 1.563, 3.0, 4.0, 5.16, 8.0, 15.0, 25.0, 50.0, 75.0, 100.0))
+        self.Q_p1 = np.array((170.7, 153.1, 127.6, 114.5, 103.40, 87.64, 58.35, 30.48, 6.94, 2.10, 0.78))
+        self.Q_p_2s = np.array((0.048, 0.080, 0.278, 0.445, 0.676, 1.53, 3.39, 3.97, 1.42, 0.44, 0.15))
+        self.Q_p_2p = np.array((2.262, 2.616, 2.861, 2.647, 2.677, 3.17, 3.09, 1.73, 0.37, 0.09, 0.03))
+        self.Q_p2 = self.Q_p_2s + self.Q_p_2p
+        self.Q_p_3s = np.array((0.002, 0.004, 0.005, 0.007, 0.015, 0.08, 0.52, 0.98, 0.46, 0.15, 0.05))
+        self.Q_p_3p = np.array((0.026, 0.047, 0.123, 0.141, 0.180, 0.30, 0.63, 0.54, 0.13, 0.03, 0.01))
+        self.Q_p_3d = np.array((0.020, 0.062, 0.120, 0.206, 0.354, 0.30, 0.17, 0.07, 0.01, 0.002, 0.001))
+        self.Q_p3 = self.Q_p_3s + self.Q_p_3p + self.Q_p_3d
+        self.units = 'energy in [keV], Q in [10^-17 cm^-2]'
+
+class cs_belkic92:
+
+    '''
+    This class holds the energy and cross sections for charge transfer 
+    from Belkic et al 1992 Phys. Rev. A. 80, Table 5. Essentially an update 
+    to Shakeshaft 1978.
+   
+    Cross sections are of collisions between protons and neutral H to 
+
+    1s : Q_p1
+    2s : Q_p_2s
+    2p : Q_p_2p
+    n=2: sum of 2s and 2p states, Q_p2
+    3s : Q_p_3s
+    3p : Q_p_3p
+    3d : Q_p_3d
+    n=3 : sum of 3s, 3p, 3d states, Q_p3
+ 
+    Energy range  1 -- 100 keV
+
+    '''
+    def __init__(self):
+        self.energy = np.array((40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 125.0, 150.0, 
+                                200.0, 300.0, 400, 500.0, 600.0, 700., 800.0, 900.0, 1000.0))
+        self.Q_p1 = np.array((1.37e-16, 6.95e-17, 3.87e-17, 2.30e-17, 1.45e-17, 9.45e-18, 6.39e-18, 2.70e-18, 1.29e-18,
+                              3.75e-19, 5.89e-20, 1.47e-20, 4.83e-21, 1.90e-21, 8.55e-22, 4.24e-22, 2.27e-22, 1.29e-22))*1e17
+        self.Q_p_2s = np.array((2.81e-17, 1.44e-17, 8.00e-18, 4.71e-18, 2.91e-18, 1.87e-18, 1.24e-18, 5.05e-19, 2.33e-19,
+                                6.43e-20, 9.38e-21, 2.24e-21, 7.13e-22, 2.75e-22, 1.22e-22, 5.96e-23, 3.16e-23, 1.79e-23))*1e17
+        self.Q_p_2p = np.array((1.12e-17, 5.70e-18, 3.08e-18, 1.75e-18, 1.05e-18, 6.48e-19, 4.15e-19, 1.54e-19, 6.51e-20,
+                                1.55e-20, 1.80e-21, 3.63e-22, 1.01e-22, 3.53e-23, 1.43e-23, 6.52e-24, 3.25e-24, 1.74e-24))*1e17
+        self.Q_p2 = np.array((3.94e-17, 2.01e-17, 1.11e-17, 6.46e-18, 3.96e-18, 2.52e-18, 1.66e-18, 6.59e-19, 2.98e-19,
+                              7.98e-20, 1.12e-20, 2.60e-21, 8.14e-22, 3.10e-22, 1.36e-22, 6.61e-23, 3.49e-23, 1.96e-23))*1e17
+        self.Q_p_3s = np.array((8.96e-18, 4.65e-18, 2.59e-18, 1.53e-18, 9.43e-19, 6.06e-19, 4.02e-19, 1.62e-19, 7.41e-20,
+                                2.03e-20, 2.91e-21, 6.88e-22, 2.18e-22, 8.38e-23, 3.69e-23, 1.18e-23, 9.55e-24, 5.39e-24))*1e17
+        self.Q_p_3p = np.array((3.85e-18, 2.01e-18, 1.11e-18, 6.39e-19, 3.84e-19, 2.39e-19, 1.54e-19, 5.72e-20, 2.43e-20,
+                                5.49e-21, 6.72e-22, 1.36e-22, 3.80e-23, 1.32e-23, 5.38e-24, 2.46e-24, 1.23e-24, 6.60e-25))*1e17
+        self.Q_p_3d = np.array((1.02e-18, 4.39e-19, 2.11e-19, 1.10e-19, 6.05e-20, 3.51e-20, 2.13e-20, 7.04e-21, 2.74e-21,
+                                5.82e-22, 6.05e-23, 1.18e-23, 3.33e-24, 1.18e-24, 4.95e-25, 2.33e-25, 1.20e-25, 6.67e-26))*1e17
+        self.Q_p3 = np.array((1.38e-17, 7.10e-18, 3.91e-18, 2.28e-18, 1.39e-18, 8.80e-19, 5.77e-19, 2.26e-19, 1.01e-19,
+                              2.66e-20, 3.64e-21, 8.35e-22, 2.59e-22, 9.82e-23, 4.28e-23, 2.07e-23, 1.09e-23, 6.11e-24))*1e17
+        self.units = 'energy in [keV], Q in [10^-17 cm^-2]'
+
+       
+
+def cs_polyfit(energy, csec, emin = -100.0, emax=-100.0, 
+               order = 3, log10E=False, log10Q = False):
+
+    if emax == -100.0:
+        emax = energy[-1]
+    if emin == -100.0:
+        emin = energy[0]
+
+    eind1 = np.where(energy >= emin)[0][0]
+    eind2 = np.where(energy <= emax)[0][-1]+1
+
+    if log10E == True:
+        energy = np.log10(energy[eind1:eind2])
+    else:
+        energy = energy[eind1:eind2]
+
+    if log10Q == True:
+        csec = np.log10(csec[eind1:eind2])
+    else:
+        csec = csec[eind1:eind2]
+
+
+    pfit = Poly.fit(energy, csec, order)
+    pfit_vals = Poly(pfit.convert().coef)    
+
+    return pfit_vals
+
+
+def cs_chebfit(energy, csec, emin = -100.0, emax=-100.0, 
+               order = 3, log10E=False, log10Q = False):
+
+    if emax == -100.0:
+        emax = energy[-1]
+    if emin == -100.0:
+        emin = energy[0]
+
+    eind1 = np.where(energy >= emin)[0][0]
+    eind2 = np.where(energy <= emax)[0][-1]+1
+
+    if log10E == True:
+        energy = np.log10(energy[eind1:eind2])
+    else:
+        energy = energy[eind1:eind2]
+
+    if log10Q == True:
+        csec = np.log10(csec[eind1:eind2])
+    else:
+        csec = csec[eind1:eind2]
+
+
+    # x = (np.log(energy/emin) - np.log(emax/energy))/np.log(emax/emin)
+
+    cfit = Cheb.fit(energy,csec,order)
+    # cfit = Cheb.fit(x, csec, order)
+    cfit_vals = Cheb(cfit.convert().coef)
+
+    return cfit_vals
+
+def cs_hyberbolfit(energy, csec, emin = -100.0, emax = -100.0,
+                   boundsin = [0, 1.1, 0, 1, 0, np.inf]):
+    
+    if emax == -100.0:
+        emax = energy[-1]
+    if emin == -100.0:
+        emin = energy[0]
+
+    eind1 = np.where(energy >= emin)[0][0]
+    eind2 = np.where(energy <= emax)[0][-1]+1
+
+    energy = energy[eind1:eind2]
+    csec = csec[eind1:eind2]
+
+    y0_guess = csec[0] 
+
+    popt_hyp, pcov_hyp=curve_fit(hyperbolic_fn, 
+                             energy, csec, 
+                             bounds=[[y0_guess*boundsin[0],boundsin[2],boundsin[4]],
+                                     [y0_guess*boundsin[1],boundsin[3],boundsin[5]]])
+
+    # yvals = OZpy.CrossSections.hyperbolic_fn(xvals, 
+                                  # *popt_hyp)
+
+    return popt_hyp, pcov_hyp
+
+def cs_expfit(energy, csec, emin = -100.0, emax = -100.0,
+              boundsin = [0.5,2.5,-10,10]):
+    
+    if emax == -100.0:
+        emax = energy[-1]
+    if emin == -100.0:
+        emin = energy[0]
+
+    eind1 = np.where(energy >= emin)[0][0]
+    eind2 = np.where(energy <= emax)[0][-1]+1
+
+    energy = energy[eind1:eind2]
+    csec = csec[eind1:eind2]
+
+    y0_guess = csec[0] 
+
+
+    popt_exp, pcov_exp=curve_fit(exponential_fn, 
+                             energy, csec,
+                             bounds=[[y0_guess*boundsin[0],boundsin[2]],[y0_guess*boundsin[1],boundsin[3]]])
+
+    #yvals = OZpy.CrossSections.exponential_fn(xvals, 
+                                  # *popt_exp)
+
+    return popt_exp, pcov_exp
+
+
+def hyperbolic_fn(x, y0, b, b0):
+    '''
+    Hyperbolic decline curve equation
+    Arguments:
+        x: Float. 
+           Energy, usually in keV
+        y0: Float. 
+            Initial value of y at start of curve.
+        b: Float. 
+            Hyperbolic decline constant
+        b0: Float. 
+            Nominal decline rate at time x=0
+    Output: 
+        q: Float.
+           The value of the function at value x
+    '''
+    return y0/((1.0+b*b0*x)**(1.0/b))
+
+
+def exponential_fn(x, y0, b):
+    """
+    Exponential decline curve equation
+    Arguments:
+         x: Float. 
+           Energy, usually in keV
+        y0: Float. 
+            Initial value of y at start of curve.
+        b: Float. 
+            Decline constant
+    Output: 
+        q: Float.
+           The value of the function at value x
+    """
+    return y0*np.exp(-b*x)
+ 
